0ce26df2963af0ec59916f25e722d2f0006a935b
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-02-20 21:21:51 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,9 @@
 def using_while
   levitation_force = 6
-	levitation_force += 1
   while levitation_force == 10
+    levitation_force += 1
     puts "Wingardium Leviosa"
+
   end
   #your code here
 end
